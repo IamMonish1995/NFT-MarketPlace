43ae69d1a21adf53159ae1ed8af3f95542246188
monish <monishbarse9@gmail.com>
2023-05-17 13:58:12 +0530

    payment added

--- screens/paymentScreen.js
+++ screens/paymentScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -6,20 +6,37 @@ import {
   Image,
   StatusBar,
   FlatList,
+  StyleSheet
 } from "react-native";
+import { WebView } from "react-native-webview";
 
 import { COLORS, SIZES, assets, SHADOWS, FONTS } from "../constants";
 import {
-  CircleButton,
-  RectButton,
-  SubInfo,
-  DetailsDesc,
-  DetailsBid,
-  FocusedStatusBar,
+  FocusedStatusBar, RectButton,
 } from "../components";
 
 const Payments = ({ route, navigation }) => {
   const { amount } = route.params;
+  const [showPaymentScreen, setShowPaymentScreen] = useState(false);
+
+  if (showPaymentScreen) {
+    const razorpayCheckoutURL =
+      `https://api.razorpay.com/v1/checkout/embedded?` +
+      `key_id=rzp_test_qYweUy4ve385yD` +
+      `&amount=100` +
+      `&currency=INR` +
+      `&name=Sample%20App` +
+      `&description=Sample%20Description` +
+      `&prefill[name]=John%20Doe` +
+      `&prefill[contact]=9999999999` +
+      `&callback_url=http://localhost:8080/paymentcallback`;
+
+    return (
+      <View style={styles.container}>
+        <WebView source={{ uri: razorpayCheckoutURL }} style={styles.webview} />
+      </View>
+    );
+  }
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -28,20 +45,20 @@ const Payments = ({ route, navigation }) => {
         backgroundColor="transparent"
         translucent={true}
       />
-
       <View
         style={{
           width: "100%",
+          height:"100%",
           position: "absolute",
-          bottom: "50%",
+          bottom: "3%",
           paddingVertical: SIZES.font,
           justifyContent: "center",
           alignItems: "center",
-          backgroundColor: "rgba(255,255,255,0.5)",
-          zIndex: 1,
+          backgroundColor: "rgba(255,0,255,0.5)",
+          zIndex: 100000,
         }}
       >
-        <Text
+        {/* <Text
           style={{
             fontSize: SIZES.font,
             fontFamily: FONTS.semiBold,
@@ -49,10 +66,29 @@ const Payments = ({ route, navigation }) => {
           }}
         >
           {amount}
-        </Text>
-      </View>
+        </Text> */}
+
+        <RectButton
+          handlePress={() =>{setShowPaymentScreen(true)}}
+          text={`Pay Now ${amount}`}
+          minWidth={170}
+          fontSize={SIZES.large}
+          {...SHADOWS.dark}
+        />
+
+        </View>
     </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+  width:"100%",
+  height:"100%"
+  },
+  webview: {
+  width:"100%",
+  height:"100%"
+  },
+});
 
 export default Payments;
